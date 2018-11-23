588cfcc95ace29d81cc2cd9b72974fc8a1156bd8
WuJiaQi <WuJiaQi@example.com>
2018-11-23 20:54:43 +0800

    更新了头部点击事件

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,14 @@
 
 import  '../less/index.less';
+
+const navLiNodes = document.querySelectorAll('.nav li');
+// const navLiNodes = document.querySelectorAll('#rap #header .hearder-main .nav li');
+
+for (var i = 0; i < navLiNodes.length; i++) {
+  navLiNodes[i].onclick = function () {
+    for (var j = 0; j < navLiNodes.length; j++) {
+      navLiNodes[j].className = '';
+    }
+    this.className = 'active';
+  };
+}
